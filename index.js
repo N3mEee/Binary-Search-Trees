2f010f0d248d08161a480539fe58ac02f80767bd
N3mEee <n3mebusiness@gmail.com>
2023-02-04 17:13:49 +0100

    add more functions to the bst

--- index.js
+++ index.js
@@ -26,6 +26,47 @@ class Tree {
         const sortedArray = [...new Set(arr.sort((a, b) => a - b))];
         return sortedArray;
     }
+
+    insert(data, root = this.root) {
+        if (root === null) return new Node(data);
+        if (root.data < data) {
+            root.right = this.insert(data, root.right);
+        } else {
+            root.left = this.insert(data, root.left);
+        }
+        return root;
+    }
+
+    #minValue(root) {
+        let minv = root.key;
+        while (root.left != null) {
+            minv = root.left.key;
+            root = root.left;
+        }
+        return minv;
+    }
+
+    delete(data, root = this.root) {
+        if (root === null) return root;
+        if (root.data < data) root.right = this.delete(data, root.right);
+        else if (root.data > data) root.left = this.delete(data, root.left);
+        else {
+            if (root.left === null) return root.right;
+            else if (root.right === null) return root.left;
+            root.data = this.#minValue(root.right);
+            root.right = this.delete(data, root.right);
+        }
+        return root;
+    }
+
+    find(data, root = this.root) {
+        const node = root;
+        if (node === null) return null;
+        if (node.data !== data) {
+            return node.data < data ? this.find(data, node.right) : this.find(data, node.left);
+        }
+        return node;
+    }
 }
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
@@ -41,7 +82,5 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
 const newTree = new Tree(arr);
-console.log(newTree);
 prettyPrint(newTree.root);
-
-//git test new ssh
+console.log(newTree.find(7));
